074b16e7efb7d11d518dde53a67f299d6beea050
Jimmy Guarin <jimmyguarin43@gmail.com>
2022-02-18 14:14:56 -0500

    Add multiple changes for pdf-service
    Support base64 html input
    Make bucket expire objects after min allowed time (1d)
    Accept width and height config
    Add CORS configuration

--- handler.py
+++ handler.py
@@ -2,6 +2,8 @@ import json
 import pdfkit
 import boto3
 import os
+import base64
+
 client = boto3.client('s3')
 
 # Get the bucket name environment variables to use in our code
@@ -10,8 +12,10 @@ S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 def generate_pdf(event, context):
     
     # Defaults
-    key = 'deafult-filename.pdf'
+    key = 'default-filename.pdf'
     html = "<html><head></head><body><h1>It works! This is the default PDF.</h1></body></html>"
+    w = '215.9'
+    h = '279.4'
     
     # TODO: Validate filename and html exist
     # TODO: Clean the filename
@@ -22,14 +26,29 @@ def generate_pdf(event, context):
     if 'body' in event:
         data = json.loads(event['body'])
         key = data['filename']
-        html = data['html'] 
+        base64_message = data['html']
+        base64_bytes = base64_message.encode('utf-8')
+        message_bytes = base64.b64decode(base64_bytes)
+        html = message_bytes.decode('utf-8')
+        w = data['width']
+        h = data['height']
+
 
     # Set file path to save pdf on lambda first (temporary storage)
     filepath = '/tmp/{key}'.format(key=key)
     
+    options = {
+        'margin-top': '0',
+        'margin-right': '0',
+        'margin-bottom': '0',
+        'margin-left': '0',
+        'orientation': 'Portrait',
+        'page-width': w,
+        'page-height': h
+    }
     # Create PDF
     config = pdfkit.configuration(wkhtmltopdf="binary/wkhtmltopdf")
-    pdfkit.from_string(html, filepath, configuration=config, options={})
+    pdfkit.from_string(html, filepath, configuration=config, options=options)
     
 
     # Upload to S3 Bucket
